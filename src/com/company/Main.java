56cb8b117a5a9886ccc9430f6153668366cce395
twildasin <tjwild127@aol.com>
2018-12-13 17:04:39 -0500

    Final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 import java.util.*;
 import javax.script.*;
+import java.io.*;
 
 
 
@@ -12,6 +13,7 @@ public class Main {
         Scanner ab = new Scanner(System.in);
         System.out.println("Input an expression");
         String expression = ab.nextLine();
+        String finalex = expression;
         expression = expression.toUpperCase();
 
 
@@ -27,12 +29,50 @@ public class Main {
         expression = expression.replaceAll("B", "1");
         */
 
-        System.out.println("A\t" + "B\t" + expression);
+        System.out.println("A\t" + "B\t" + expression + "\n\n");
 
-        System.out.println(solver.solve(expression, 0, 0));
+        /*System.out.println(solver.solve(expression, 0, 0));
         System.out.println(solver.solve(expression, 0, 1));
         System.out.println(solver.solve(expression, 1, 0));
-        System.out.println(solver.solve(expression, 1, 1));
+        System.out.println(solver.solve(expression, 1, 1));*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        try {
+            //Whatever the file path is.
+            File statText = new File("output.txt");
+            FileOutputStream is = new FileOutputStream(statText);
+            OutputStreamWriter osw = new OutputStreamWriter(is);
+            Writer w = new BufferedWriter(osw);
+            w.write("Truth Table for expression: " + finalex + "\n\n"+ "A\tB\tOUTPUT\n0\t0\t  ");
+
+            w.write((solver.solve(expression, 0, 0) == 0) ? "0" : "1");
+            w.write("\n0\t1\t  ");
+
+            w.write((solver.solve(expression, 0, 1) == 0) ? "0" : "1");
+            w.write("\n1\t0\t  ");
+
+            w.write((solver.solve(expression, 1, 0) == 0) ? "0" : "1");
+            w.write("\n1\t1\t  ");
+
+            w.write((solver.solve(expression, 1, 1) == 0) ? "0" : "1");
+            w.close();
+        } catch (IOException e) {
+            System.err.println("Problem writing to the file statsTest.txt");
+        }
+
+
 
     }
 
--- src/com/company/solver.java
+++ src/com/company/solver.java
@@ -3,6 +3,7 @@ package com.company;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.Objects;
 
 /**
  * Created by tw073 on 12/11/18.
@@ -30,17 +31,55 @@ public class solver {
 
 
 
+        int startp = 0;
+        int endp = 0;
+        int nump = 1;
+        int place = 0;
+        String temp = "(0*0)";
+        if(str.indexOf('!') !=-1)
+        {
+            //System.out.println("OUTPTPTTP   " + str.indexOf('(', str.indexOf('!')));
+            startp = str.indexOf('(', str.indexOf('!'));
+            place = startp;
+            System.out.println("Negation Found, Checking for char in place: ");
+            while (nump != 0)
+            {
+                place++;
+                if(Objects.equals(str.charAt(place) , '('))                     ///Might have to use Objects equal
+                    nump++;
+                if((Objects.equals(str.charAt(place) , ')')))
+                    nump--;
+                System.out.println(place);
+
+            }
+            endp = place + 1;
+
+
+            temp = str.substring(startp, endp );
+            //temp = str.substring(1, 6);
+
+            System.out.println("Start position of \"(\" : " + startp + "\tEnd position of \")\" : " + endp + "\t Full expression" + str + "\tTEMP (Expression to send back into method to be solved) - " + temp + "\n");
+            str = str.substring(0,startp - 1) +           ((solver.solve(temp, a, b) == 0) ? 1 : 0)         + str.substring(endp,str.length()) ;
+            System.out.println("REVISED (after substituting)   " + str);
+        }
+
+
+
+
+
+
+
 
         ScriptEngine engine = new ScriptEngineManager().getEngineByExtension("js");
 
         int end = -1;
-        System.out.println(str);
+        //System.out.println(str);
 
         try {
             // Evaluate the expression
             Object result = engine.eval(str);
 
-            System.out.println(result);
+            //System.out.println(result);
             if(result.equals(0) ){
                 end = 0;
             }else if (!result.equals(0)){
