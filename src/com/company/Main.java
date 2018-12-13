7477d09e6da0526819a846dfaa00a77657e7a05a
Tanner Wildasin <tw073@pvsd.org>
2018-12-13 11:12:53 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,37 +8,34 @@ import javax.script.*;
 public class Main {
 
     public static void main(String[] args) /*throws ScriptException*/{
-Scanner ab = new Scanner(System.in);
+
+        Scanner ab = new Scanner(System.in);
+        System.out.println("Input an expression");
+        String expression = ab.nextLine();
+        expression = expression.toUpperCase();
 
 
 
-        String expression = "(A && B) || A";
         expression = expression.replace(" ", "");
         expression = expression.replaceAll("&&", "*");
         expression = expression.replaceAll("\\|\\|", "+");
-        expression = expression.replaceAll("!A", "0");
+        /*expression = expression.replaceAll("!A", "0");
         expression = expression.replaceAll("!B", "0");
         expression = expression.replaceAll("A", "1");
         expression = expression.replaceAll("B", "1");
         expression = expression.replaceAll("A", "1");
         expression = expression.replaceAll("B", "1");
+        */
+
 
         System.out.println(expression);
 
-        // Get JavaScript engine
-        ScriptEngine engine = new ScriptEngineManager().getEngineByExtension("js");
 
-        try {
-            // Evaluate the expression
-            Object result = engine.eval(expression);
+        System.out.println(solver.solve(expression, 0, 0));
 
-            System.out.println(result);
-        }
-        catch (ScriptException e) {
-            // Something went wrong
-            //e.printStackTrace();
-            System.out.println("Error");
-        }
 
     }
+
+
+
 }
--- src/com/company/solver.java
+++ src/com/company/solver.java
@@ -1,10 +1,63 @@
 package com.company;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 /**
  * Created by tw073 on 12/11/18.
  */
 public class solver {
+
+    private String str;
+
     public solver(String a){
 
+        str = a;
+
     }
+
+    public static int solve (String str, int a, int b)
+    {
+
+
+
+        str = str.replaceAll("!A", (a == 0) ? "1" : "0");
+        str = str.replaceAll("!B", (b == 0) ? "1" : "0");
+
+        str = str.replaceAll("A", Integer.toString(a));
+        str = str.replaceAll("B", Integer.toString(b));
+
+
+
+
+        ScriptEngine engine = new ScriptEngineManager().getEngineByExtension("js");
+
+        int end = -1;
+        System.out.println(str);
+
+        try {
+            // Evaluate the expression
+            Object result = engine.eval(str);
+
+            System.out.println(result);
+            if(result.equals(0) ){
+                end = 0;
+            }else if (!result.equals(0)){
+                end = 1;
+            }
+        }
+        catch (ScriptException e) {
+            // Something went wrong
+            //e.printStackTrace();
+            System.out.println("Error");
+            end = -1;
+        }
+        return end;
+
+
+
+    }
+
+
 }
